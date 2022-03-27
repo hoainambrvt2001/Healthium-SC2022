51dac2be3436df90af9c43f6330db93e7aef8727
tuniss1 <77372423+tuniss1@users.noreply.github.com>
2022-03-27 14:19:13 +0700

    Update

--- components/Notification_list_udpate/Noti.js
+++ components/Notification_list_udpate/Noti.js
@@ -5,7 +5,8 @@ import {
   FlatList,
   TouchableWithoutFeedback,
   ScrollView,
-  Text, Linking
+  Text,
+  Linking,
 } from "react-native";
 import { Title, Paragraph } from "react-native-paper";
 import ServiceOption from "components/HomeScreen/ServiceOption";
@@ -15,78 +16,97 @@ import { listNotes, listServices } from "components/Utils/CLONEDATA";
 const Noti = ({ navigation }) => {
   return (
     <View style={{ flex: 1 }}>
-
       <TouchableWithoutFeedback
         // onPress={() => navigation.navigate("NotificationScreen")}
-        onPress={() => Linking.openURL('https://edition.cnn.com/2022/03/24/health/evusheld-unused/index.html')}
+        onPress={() =>
+          Linking.openURL(
+            "https://edition.cnn.com/2022/03/24/health/evusheld-unused/index.html"
+          )
+        }
       >
         <View
           style={{
             flexDirection: "row",
             alignItems: "baseline",
-            elevation: 4,
+
             backgroundColor: "#fafafa",
             marginVertical: 10,
             marginHorizontal: 25,
             padding: 10,
             borderRadius: 10,
+
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.23,
+            shadowRadius: 2.62,
+            elevation: 4,
           }}
         >
           <Image
             source={require("assets/sms-icon.png")}
-            style={{ width: 20, height: 20, marginHorizontal: 5 }}
+            style={{ width: 20, height: 20, marginLeft: 5 }}
           />
           <View
             style={{
-              marginLeft: 5,
-              minHeight: 100,
+              flex: 1,
+              marginLeft: 10,
             }}
-            
           >
             <Title>Health Check</Title>
             <Paragraph numberOfLines={4}>
-                Vulnerable Americans are desperate to find 
-                this Covid-19 drug. Thousands of boxes are sitting around unused.
-                
+              Vulnerable Americans are desperate to find this Covid-19 drug.
+              Thousands of boxes are sitting around unused.
             </Paragraph>
           </View>
         </View>
       </TouchableWithoutFeedback>
 
-
-
       <TouchableWithoutFeedback
         // onPress={() => navigation.navigate("NotificationScreen")}
-        onPress={() => Linking.openURL('https://edition.cnn.com/2022/03/24/health/evusheld-unused/index.html')}
+        onPress={() =>
+          Linking.openURL(
+            "https://edition.cnn.com/2022/03/24/health/evusheld-unused/index.html"
+          )
+        }
       >
         <View
           style={{
             flexDirection: "row",
             alignItems: "baseline",
-            elevation: 4,
+
             backgroundColor: "#fafafa",
             marginVertical: 10,
             marginHorizontal: 25,
             padding: 10,
             borderRadius: 10,
+
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.23,
+            shadowRadius: 2.62,
+            elevation: 4,
           }}
         >
           <Image
             source={require("assets/sms-icon.png")}
-            style={{ width: 20, height: 20, marginHorizontal: 5 }}
+            style={{ width: 20, height: 20, marginLeft: 5 }}
           />
           <View
             style={{
-              marginLeft: 5,
-              minHeight: 100,
+              flex: 1,
+              marginLeft: 10,
             }}
-            
           >
             <Title>Health Check</Title>
             <Paragraph numberOfLines={4}>
-                Vulnerable Americans are desperate to find 
-                this Covid-19 drug. Thousands of boxes are sitting around unused.
-                
+              Vulnerable Americans are desperate to find this Covid-19 drug.
+              Thousands of boxes are sitting around unused.
             </Paragraph>
           </View>
         </View>
